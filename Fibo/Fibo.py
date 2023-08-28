c09e0489f8f8c06821627b4e9595eec130a9ded7
MisakAvagyan <avagyan777555@gmail.com>
2023-08-28 12:58:48 +0400

    Revert "Revert "Added new file""
    This reverts commit 26fb2903227f1c6c21104632fe40f8f96178a26b.

---
+++ Fibo/Fibo.py
@@ -0,0 +1,18 @@
+def numbers(num):
+    if num == 1:
+        return 0
+    elif num == 2:
+        return 1
+    elif num == 3:
+        return 1
+    else:
+        return numbers(num - 1) + numbers(num - 2)
+
+
+def main():
+    num = int(input('enter a number - '))
+    print('Fibonacci number is - ', numbers(num))
+
+
+if __name__ == '__main__':
+    main()
