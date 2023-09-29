e86594c94e873701b3a8b3ddc0cb4c6b77cb7291
MisakAvagyan <140651600+MisakAvagyan@users.noreply.github.com>
2023-09-29 19:50:24 +0400

    Update script.py

--- Send_mail/script.py
+++ Send_mail/script.py
@@ -6,7 +6,7 @@ from getpass import getpass
 def send_mail(text):
     email = input('Enter your email - ')
     recipient_email = input('Enter the recipient_email - ')
-    password = getpass("Enter your Gmail password (or application-specific password): ")
+    password = getpass("Enter your Gmail password (or application-specific password) - ")
 
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
